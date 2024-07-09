182ef404a82c8169da384b97ac4dfec6bcec76f1
Merhawi <merhawitsegay007@gmail.com>
2024-07-08 17:27:30 -0700

    add those experimental nb

--- src/Network.py
+++ src/Network.py
@@ -77,35 +77,38 @@ class CustomVGG(nn.Module):
         # Convolutional layers
         self.features = nn.Sequential(
             # First block
-            nn.Conv2d(1, 64, kernel_size=3, padding=1),
+            nn.Conv2d(1, 32, kernel_size=3, padding=1),
             nn.ReLU(inplace=True),
-            nn.Conv2d(64, 64, kernel_size=3, padding=1),
+            nn.BatchNorm2d(32),
+            nn.Conv2d(32, 32, kernel_size=3, padding=1),
+            nn.BatchNorm2d(32),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=2, stride=2),
             
             # Second block
-            nn.Conv2d(64, 128, kernel_size=3, padding=1),
+            nn.Conv2d(32, 64, kernel_size=3, padding=1),
+            nn.BatchNorm2d(64),
             nn.ReLU(inplace=True),
-            nn.Conv2d(128, 128, kernel_size=3, padding=1),
+            nn.Conv2d(64, 64, kernel_size=3, padding=1),
+            nn.BatchNorm2d(64),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=2, stride=2),
             
-            # Third block
-            nn.Conv2d(128, 256, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(256, 256, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=2, stride=2),
+            # # Third block
+            # nn.Conv2d(128, 256, kernel_size=3, padding=1),
+            # nn.BatchNorm2d(256),
+            # nn.ReLU(inplace=True),
+            # nn.Conv2d(256, 256, kernel_size=3, padding=1),
+            # nn.BatchNorm2d(256),
+            # nn.ReLU(inplace=True),
+            # nn.MaxPool2d(kernel_size=2, stride=2),
         )
         
         # Fully connected layers
         self.classifier = nn.Sequential(
-            nn.Linear(256 * 3 * 3, 512),
+            nn.Linear(64 * 7 * 7, 512),
             nn.ReLU(inplace=True),
-            nn.Dropout(),
-            nn.Linear(512, 512),
-            nn.ReLU(inplace=True),
-            nn.Dropout(),
+            nn.Dropout(0.3),
             nn.Linear(512, num_classes)
         )
 
